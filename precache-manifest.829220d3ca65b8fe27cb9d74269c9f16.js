0cfe09ebcacfaec307604fb2fc4c8fcf979ff2fa
JJ-Hall <joshhall1991@gmail.com>
2020-08-17 01:54:37 -0500

    Updates

---
+++ precache-manifest.829220d3ca65b8fe27cb9d74269c9f16.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e047b7368960ffaffb775ec1688b949",
+    "url": "/PorfolioSite/index.html"
+  },
+  {
+    "revision": "5132b65d9335ef586aca",
+    "url": "/PorfolioSite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "baadb406b0abbf08d548",
+    "url": "/PorfolioSite/static/js/2.89fcbe41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PorfolioSite/static/js/2.89fcbe41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5132b65d9335ef586aca",
+    "url": "/PorfolioSite/static/js/main.a41b4642.chunk.js"
+  },
+  {
+    "revision": "d3af2529a433a641dddb",
+    "url": "/PorfolioSite/static/js/runtime-main.7afe1d76.js"
+  },
+  {
+    "revision": "dbbb95f01e3b5711d388edca216fb0ae",
+    "url": "/PorfolioSite/static/media/Travelsite.dbbb95f0.JPG"
+  },
+  {
+    "revision": "5f57f6045b8b936a1d23dfe1952fb59c",
+    "url": "/PorfolioSite/static/media/aboutMeButton.5f57f604.svg"
+  },
+  {
+    "revision": "a2353b04fab541a12ec0fc9d1add938e",
+    "url": "/PorfolioSite/static/media/contactButton.a2353b04.svg"
+  },
+  {
+    "revision": "2a416c3449edac25c2e65fa6accb2ce9",
+    "url": "/PorfolioSite/static/media/github.2a416c34.svg"
+  },
+  {
+    "revision": "065f72317d73b100b41f2f2b08d01813",
+    "url": "/PorfolioSite/static/media/homeButton.065f7231.svg"
+  },
+  {
+    "revision": "405ec938ada74412a13b8a78f81e4618",
+    "url": "/PorfolioSite/static/media/izuddin-h-bg.405ec938.jpg"
+  },
+  {
+    "revision": "aa4ad851c239959b000d02231e48e854",
+    "url": "/PorfolioSite/static/media/projectButton.aa4ad851.svg"
+  }
+]);
